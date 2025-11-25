324c69f10584c5dbfffc920379a1b3e9f1099fa5
Shazib Siddique <siddiqueshazib4@gmail.com>
2025-11-25 02:28:43 -0500

    Update OLLAMA_MODEL to llama3.2:latest to match AWS installation

--- financial_site/settings.py
+++ financial_site/settings.py
@@ -45,7 +45,7 @@ CSRF_TRUSTED_ORIGINS = [
 
 # Ollama settings
 OLLAMA_API_BASE = os.getenv('OLLAMA_API_BASE', 'http://localhost:11434')
-OLLAMA_MODEL = os.getenv('OLLAMA_MODEL', 'llama3.1')
+OLLAMA_MODEL = os.getenv('OLLAMA_MODEL', 'llama3.2:latest')
 
 # Vector DB
 VECTOR_DB_PATH = os.getenv('VECTOR_DB_PATH', 'chrome_langchain_db')
